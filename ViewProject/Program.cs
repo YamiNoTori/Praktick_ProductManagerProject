0e71952082579e484b06d029ff96dae3535ff15a
Dmitriy <YamiNoTori>
2022-05-19 14:12:03 +0300

    init solution and projects with MVC

---
+++ ViewProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display the number of command line arguments.
+            Console.WriteLine(args.Length);
+        }
+    }
+}
